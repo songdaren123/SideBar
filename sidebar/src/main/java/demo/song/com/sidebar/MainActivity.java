bdb6de0d27d96521175f054981039382b87cb162
songmingzhan <songmingzhan-v@rongcloud.cn>
2018-08-17 12:05:24 +0800

    问题:实现索引 原因: 解决方案: Reviewer: 工单号:

--- sidebar/src/main/java/demo/song/com/sidebar/MainActivity.java
+++ sidebar/src/main/java/demo/song/com/sidebar/MainActivity.java
@@ -3,12 +3,12 @@ package demo.song.com.sidebar;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -16,27 +16,37 @@ import java.util.Comparator;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private final  String TAG=MainActivity.class.getSimpleName();
+    private final String TAG = MainActivity.class.getSimpleName();
     private SideBar mSideBar;
     private List<CityInfo> citys;
     private RecyclerView mRecyclerView;
+    private CityAdapter mCityAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initData();
-        mSideBar=findViewById(R.id.sidebar);
-        mRecyclerView=findViewById(R.id.recycelview);
+        mSideBar = findViewById(R.id.sidebar);
+        mRecyclerView = findViewById(R.id.recycelview);
         mSideBar.setItemSelectedListener(new SideBar.SideBarItemSelectedListener() {
             @Override
             public void itemSelected(String str) {
-                Log.i(TAG, "itemSelected: "+str);
+                int index = mCityAdapter.getIndex(str);
+                if (index != -1) {
+                    mRecyclerView.scrollToPosition(index);
+                }
+
             }
         });
-        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         mRecyclerView.setLayoutManager(linearLayoutManager);
+        mCityAdapter = new CityAdapter();
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        mRecyclerView.setAdapter(mCityAdapter);
     }
+
     private void initData() {
         citys = new ArrayList<>();
         citys.add(new CityInfo("北京", 1));
@@ -49,19 +59,33 @@ public class MainActivity extends AppCompatActivity {
         citys.add(new CityInfo("河北", 8));
         citys.add(new CityInfo("广东", 9));
         citys.add(new CityInfo("广西", 10));
+        citys.add(new CityInfo("安徽", 11));
+        citys.add(new CityInfo("江苏", 12));
+        citys.add(new CityInfo("福建", 13));
+        citys.add(new CityInfo("湖南", 14));
+        citys.add(new CityInfo("湖北", 15));
+        citys.add(new CityInfo("浙江", 16));
+        citys.add(new CityInfo("四川", 17));
+        citys.add(new CityInfo("云南", 18));
+        citys.add(new CityInfo("台湾", 19));
+        citys.add(new CityInfo("辽宁", 20));
+        citys.add(new CityInfo("吉林", 21));
+        citys.add(new CityInfo("黑龙江", 22));
+        citys.add(new CityInfo("西藏", 23));
+        citys.add(new CityInfo("青海", 24));
+        citys.add(new CityInfo("新疆", 25));
+        citys.add(new CityInfo("宁夏", 26));
         for (CityInfo city : citys) {
             String pinyin = CharacterParser.getInstance().getSelling(city.getName());
-            String letter=pinyin.substring(0,1);
+            String letter = pinyin.substring(0, 1);
             city.setLetter(letter.toUpperCase());
         }
-        Collections.sort(citys,new PinyinComparable());
+        Collections.sort(citys, new PinyinComparable());
 
 
     }
 
     public class PinyinComparable implements Comparator<CityInfo> {
-
-
 
 
         @Override
@@ -74,23 +98,38 @@ public class MainActivity extends AppCompatActivity {
         @NonNull
         @Override
         public CityHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-            return null;
+            View view = android.view.LayoutInflater.from(MainActivity.this).inflate(R.layout.layout_item, viewGroup, false);
+            CityHolder holder = new CityHolder(view);
+            return holder;
         }
 
         @Override
         public void onBindViewHolder(@NonNull CityHolder cityHolder, int i) {
-
+            cityHolder.cityname.setText(citys.get(i).getName());
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return citys.size();
+        }
+
+        public int getIndex(String letter) {
+            for (CityInfo cityInfo : citys) {
+                if (cityInfo.getLetter().equals(letter)) {
+                    return citys.indexOf(cityInfo);
+                }
+            }
+            return -1;
         }
     }
 
     private class CityHolder extends RecyclerView.ViewHolder {
+        private android.widget.TextView cityname;
+
         public CityHolder(@NonNull View itemView) {
             super(itemView);
+            cityname = itemView.findViewById(R.id.tv_city);
         }
     }
+
 }
--- sidebar/src/main/java/demo/song/com/sidebar/SideBar.java
+++ sidebar/src/main/java/demo/song/com/sidebar/SideBar.java
@@ -11,38 +11,41 @@ import android.view.MotionEvent;
 import android.view.View;
 
 public class SideBar extends View {
-    private String initials[]={"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+    private String initials[] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z","#"};
     private Paint mPaint;
     private int initialHeight;
-    private int touchItem=-1;
+    private int touchItem = -1;
     private SideBarItemSelectedListener listener;
+
     public SideBar(Context context) {
         this(context, null);
     }
 
     public SideBar(Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public SideBar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         initView();
     }
-    private void initView(){
-        mPaint=new Paint();
+
+    private void initView() {
+        mPaint = new Paint();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        int height=getHeight();
-        initialHeight=height/initials.length;
-        for(int i=0;i<initials.length;i++) {
+        int height = getHeight();
+        int width = getWidth();
+        initialHeight = height / initials.length;
+        for (int i = 0; i < initials.length; i++) {
             mPaint.setColor(i == touchItem ? Color.GREEN : Color.BLUE);
             mPaint.setAntiAlias(true);
             mPaint.setTextSize(30);
-            mPaint.setTextAlign(Paint.Align.RIGHT);
-            canvas.drawText(initials[i], 100, (initialHeight * i), mPaint);
+            float pionx = (width / 2 - mPaint.measureText(initials[i]) / 2);
+            canvas.drawText(initials[i], pionx, (initialHeight * i) + initialHeight, mPaint);
             mPaint.reset();
         }
     }
@@ -53,9 +56,9 @@ public class SideBar extends View {
             case MotionEvent.ACTION_MOVE:
             case MotionEvent.ACTION_DOWN:
                 float point = event.getY();
-                int index = (int) (point / initialHeight);
-                if(listener!=null){
-                    touchItem=index;
+                int index = (int) (point / getHeight() * initials.length);
+                if (listener != null) {
+                    touchItem = index;
                     listener.itemSelected(initials[index]);
                 }
                 return true;
@@ -65,10 +68,12 @@ public class SideBar extends View {
         invalidate();
         return super.onTouchEvent(event);
     }
-    public void setItemSelectedListener(SideBarItemSelectedListener listener){
-        this.listener=listener;
+
+    public void setItemSelectedListener(SideBarItemSelectedListener listener) {
+        this.listener = listener;
     }
-    public interface SideBarItemSelectedListener{
+
+    public interface SideBarItemSelectedListener {
         void itemSelected(String str);
     }
 }
